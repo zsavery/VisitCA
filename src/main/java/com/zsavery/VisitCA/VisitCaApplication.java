b493baa755c4e9201c4ce4ace8afbe1648365dd0
zsavery <zsavery998@gmail.com>
2022-12-02 12:33:04 -0600

    Initial

---
+++ src/main/java/com/zsavery/VisitCA/VisitCaApplication.java
@@ -0,0 +1,13 @@
+package com.zsavery.VisitCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsavery/VisitCA/VisitCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsavery.VisitCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
